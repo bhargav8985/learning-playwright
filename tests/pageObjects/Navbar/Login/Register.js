1fcdcef25b294667e0288ca22fe239d894b8723b
Bhargav Reddy <bhargavreddy@gmail.com>
2026-02-09 12:40:26 +0530

    changed cucumber.js

--- tests/pageObjects/Navbar/Login/Register.js
+++ tests/pageObjects/Navbar/Login/Register.js
@@ -7,13 +7,19 @@ class Register{
         this.page=page;
         this.browser=new Browser(page);
     }
-async fillform(fname,lname,phone,country,email,password){
-      await this.browser.setValueInTextField(this.browser.getLocator(locators.RegisterForm.firstname),fname);
-    await this.browser.setValueInTextField(this.browser.getLocator(locators.RegisterForm.lastname),lname);
-    await this.browser.setValueInTextField(this.browser.getLocator(locators.RegisterForm.phone),phone);
-    await this.browser.getLocator(locators.RegisterForm.country).selectOption(country);
-    await this.browser.setValueInTextField(this.browser.getLocator(locators.RegisterForm.email),email);
-    await this.browser.setValueInTextField(this.browser.getLocator(locators.RegisterForm.password),password);
+    async goto() {
+        await this.page.goto(
+            'https://qa-practice.razvanvancea.ro/register.html',
+            { waitUntil: 'domcontentloaded', timeout: 60000 }
+        );
+    }
+async fillform(user){
+      await this.browser.setValueInTextField(this.browser.getLocator(locators.RegisterForm.firstname),user.fname);
+    await this.browser.setValueInTextField(this.browser.getLocator(locators.RegisterForm.lastname),user.lname);
+    await this.browser.setValueInTextField(this.browser.getLocator(locators.RegisterForm.phone),user.phone);
+    await this.browser.getLocator(locators.RegisterForm.country).selectOption(user.country);
+    await this.browser.setValueInTextField(this.browser.getLocator(locators.RegisterForm.email),user.email);
+    await this.browser.setValueInTextField(this.browser.getLocator(locators.RegisterForm.password),user.password);
 }
  async clickRegisterlogin(){
         await this.browser.click(this.browser.getLocator(locators.RegisterForm.registerBtn));
